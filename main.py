99cc6ed47e201af2d7312214ea68be8fda8d222f
Matthew Thomas <thomasm6883@my.uwstout.edu>
2023-11-09 22:53:28 -0600

    Clickable and now with scroll, but cant change seed

--- main.py
+++ main.py
@@ -1,63 +1,113 @@
 import networkx as nx
 import matplotlib.pyplot as plt
-import tkinter as tk
+from tkinter import *
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.backends.backend_tkagg import NavigationToolbar2Tk
 
+#increment the size of the graph using the scale,
+def incrementGraph():
+    global G, cases, node_positions, seed, node_color, labels
+    G = nx.complete_graph(seed)#set the graph to the seed graph
+    node_color = ['lightblue'] * len(G.nodes())
+    print(G)
+    x = 1
+    while x < cases.get():#find the nth cartesian product
+        print(x)
+        G = nx.cartesian_product(G, nx.complete_graph(seed))
+        node_color += ['lightblue'] * (2 ** x)
+        x += 1
+    node_positions = nx.circular_layout(G)
+    update_plot()
+#on the event of clicking a node, ensure that there is a node within the area to click and then set it. Find its index and set its color for user clarity
 def on_node_click(event):
+    global node_color, selected_node, node_positions, G
     if event.xdata is not None and event.ydata is not None:
         x, y = event.xdata, event.ydata
         for node, (nx, ny) in node_positions.items():
-            if abs(x - nx) < 0.02 and abs(y - ny) < 0.02:
-                global selected_node
+            if abs(x - nx) < 0.2 and abs(y - ny) < 0.2:
                 selected_node = node
 
+    #find the index
+    index = 0
+    for node in G:
+        print(selected_node)
+        print(node)
+        if selected_node == node:
+            break
+        index+=1
+    node_color[index] = 'red'
+    update_plot()
+
+#on the release of a node, change the coordinates and then change the color
 def on_node_release(event):
+    global selected_node, node_color, node_positions
     if selected_node is not None:
         x, y = event.xdata, event.ydata
         node_positions[selected_node] = (x, y)
+
+        print(selected_node)
+
+        #find the index
+        index = 0
+        for node in G:
+            print(selected_node)
+            print(node)
+            if selected_node == node:
+                break
+            index += 1
+        node_color[index] = 'lightblue'
+        selected_node = None
         update_plot()
 
+#update the plot by clearing the screen and redrawing
 def update_plot():
+    global G, node_color
+    print("updating plot")
     ax.clear()
-    nx.draw(G, pos=node_positions, with_labels=True, node_size=3000, node_color='lightblue')
+    nx.draw(G, pos=node_positions, with_labels=True, node_size=1010, node_color=node_color)
     canvas.draw()
 
 selected_node = None
 
 # Create the window for your project
-root = tk.Tk()
+root = Tk()
 root.wm_title("Hyper-iority")
 
 # Create a figure and axis for your NetworkX graph
 fig = plt.figure(figsize=(6, 6))
 ax = fig.add_subplot(111)
 
-x = 0
-seed = 2
+cases=IntVar()
+cases.set(1)
+seed = 3
 G = nx.complete_graph(seed)
-while x < 3:
-    G = nx.cartesian_product(G, nx.complete_graph(seed))
-    x = x + 1
-    print(x)
-
-# Plot the NetworkX graph on the Matplotlib figure
 node_positions = nx.circular_layout(G)
-nx.draw(G,node_positions, ax=ax)
+node_color=['lightblue']*len(G.nodes)
+
+
+nx.draw(G, pos=node_positions, with_labels=True, node_size=1010, node_color=node_color)
 
 # Create a Tkinter canvas to embed the Matplotlib figure
 canvas = FigureCanvasTkAgg(fig, master=root)
 canvas_widget = canvas.get_tk_widget()
-canvas_widget.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+canvas_widget.pack(side=TOP, fill=BOTH, expand=True)
 
 # Add a Matplotlib navigation toolbar (optional)
 toolbar = NavigationToolbar2Tk(canvas, root)
 toolbar.update()
-canvas_widget.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+canvas_widget.pack(side=TOP, fill=BOTH, expand=True)
 
+#add an incremental scale: label here
+scaleLabel = Label(root, text="Increment Recursive Cases")
+scaleLabel.pack()
+#scale here
+recursiveCases = Scale(root, variable=cases, from_=1, to=5, orient=HORIZONTAL, command=lambda val:incrementGraph())
+recursiveCases.pack(anchor = CENTER)
+
+#add listeners for node clicks
 canvas.mpl_connect('button_press_event', on_node_click)
 canvas.mpl_connect('button_release_event', on_node_release)
 
 # Main loop to run the Tkinter application
-tk.mainloop()
+root.mainloop()
